ed55a9db84875a2776ca6e0b2b52fba18f136d6c
root <root@pwbansi01v.hq.bn-corp.com>
2018-08-24 18:34:35 -0400

    approve groovy script

---
+++ approve_script.groovy
@@ -0,0 +1,10 @@
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
+import org.jenkinsci.plugins.scriptsecurity.scripts.languages.GroovyLanguage
+
+final ScriptApproval sa = ScriptApproval.get();
+
+String script = new File('dsl.groovy').text
+
+ScriptApproval.PendingScript s = new ScriptApproval.PendingScript(script, GroovyLanguage.get(), ApprovalContext.create())
+
+sa.approveScript(s.getHash())
